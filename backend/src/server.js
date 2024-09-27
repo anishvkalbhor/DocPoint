7b8bee4b00538a12779fd4dee26246f3adc1e993
RA-L-PH <123739852+RA-L-PH@users.noreply.github.com>
2024-09-28 02:20:59 +0530

    Refactor server.js to update the default port from 3000 to 5173

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5173;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
